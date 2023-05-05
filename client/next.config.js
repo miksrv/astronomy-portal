3b907704c50e0f9beda1f229bb591a48b3c8ea47
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2023-05-05 15:09:43 -0700

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -12,9 +12,9 @@ const nextConfig = {
         ],
         // unoptimized - When true, the source image will be served as-is instead of changing quality,
         // size, or format. Defaults to false.
-        unoptimized: false
+        unoptimized: true
     },
-    output: 'standalone',
+    // output: 'standalone',
     reactStrictMode: true
 }
 
