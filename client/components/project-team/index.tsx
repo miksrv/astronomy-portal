05cfdbddaf4c86e512b71939a402eb3d1d2769a8
Misha Topchilo <miksoft.tm@gmail.com>
2023-06-06 08:45:55 -0700

    Implemented new page - About (#69)
    * Implemented new page - About
    
    * Added photos, fixed errors
    
    * Fixed code smells
    
    ---------
    
    Co-authored-by: Misha Topchilo <20854498+miksrv@users.noreply.github.com>

---
+++ client/components/project-team/index.tsx
@@ -0,0 +1,65 @@
+import Image, { StaticImageData } from 'next/image'
+import React from 'react'
+
+import photoIgor from '@/public/photos/team-Igor.jpg'
+import photoMisha from '@/public/photos/team-Misha.jpg'
+import photoSergey from '@/public/photos/team-Sergey.jpg'
+import photoTanya from '@/public/photos/team-Tanya.jpg'
+import photoVladimir from '@/public/photos/team-Vladimir.jpg'
+import photoZhenya from '@/public/photos/team-Zhenya.jpg'
+
+import styles from './styles.module.sass'
+
+type TTeam = {
+    name?: string
+    photo?: StaticImageData
+}
+
+const team: TTeam[] = [
+    {
+        name: 'Татьяна Гавриш',
+        photo: photoTanya
+    },
+    {
+        name: 'Игорь Коммисарчик',
+        photo: photoIgor
+    },
+    {
+        name: 'Сергей Медведев',
+        photo: photoSergey
+    },
+    {
+        name: 'Михаил Топчило',
+        photo: photoMisha
+    },
+    {
+        name: 'Евгений Зимин',
+        photo: photoZhenya
+    },
+    {
+        name: 'Владимир Иванович',
+        photo: photoVladimir
+    }
+]
+
+const Team: React.FC = () => (
+    <div className={styles.section}>
+        {team?.map((item) => (
+            <div
+                key={item.name}
+                className={styles.item}
+            >
+                <Image
+                    className={styles.photo}
+                    src={item.photo?.src!}
+                    alt={item?.name ?? ''}
+                    width={item.photo?.width}
+                    height={item.photo?.height}
+                />
+                <div className={styles.title}>{item.name}</div>
+            </div>
+        ))}
+    </div>
+)
+
+export default Team
