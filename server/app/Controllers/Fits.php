2533ea6a745f94689238289e778e7eeab214b0cb
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2023-08-02 10:26:16 -0700

    Fixed issues

--- server/app/Controllers/Fits.php
+++ server/app/Controllers/Fits.php
@@ -5,6 +5,7 @@ use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\RESTful\ResourceController;
 use CodeIgniter\API\ResponseTrait;
 use ReflectionException;
+use Exception;
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, DELETE, PATCH, PUT');
@@ -40,51 +41,51 @@ class Fits extends ResourceController
             $insert = [
                 'id'        => md5($input['FILE_NAME']),
                 'file_name' => $input['FILE_NAME'],
-                'ypixsz'    => isset($input['YPIXSZ']) ? floatval($input['YPIXSZ']) : NULL,
-                'xpixsz'    => isset($input['XPIXSZ']) ? floatval($input['XPIXSZ']) : NULL,
-                'naxis1'    => isset($input['NAXIS1']) ? intval($input['NAXIS1']) : NULL,
-                'naxis2'    => isset($input['NAXIS2']) ? intval($input['NAXIS2']) : NULL,
-                'naxis'     => isset($input['NAXIS']) ? intval($input['NAXIS']) : NULL,
+                'ypixsz'    => isset($input['YPIXSZ']) ? floatval($input['YPIXSZ']) : null,
+                'xpixsz'    => isset($input['XPIXSZ']) ? floatval($input['XPIXSZ']) : null,
+                'naxis1'    => isset($input['NAXIS1']) ? intval($input['NAXIS1']) : null,
+                'naxis2'    => isset($input['NAXIS2']) ? intval($input['NAXIS2']) : null,
+                'naxis'     => isset($input['NAXIS']) ? intval($input['NAXIS']) : null,
                 'bscale'    => isset($input['BSCALE']) ? intval($input['BSCALE']) : 0,
-                'simple'    => isset($input['SIMPLE']) ? intval($input['SIMPLE']) : NULL,
-                'bitpix'    => isset($input['BITPIX']) ? intval($input['BITPIX']) : NULL,
-                'xbinning'  => isset($input['XBINNING']) ? intval($input['XBINNING']) : NULL,
-                'ybinning'  => isset($input['YBINNING']) ? intval($input['YBINNING']) : NULL,
-                'exptime'   => isset($input['EXPTIME']) ? intval($input['EXPTIME']) : NULL,
-                'frame'     => $input['FRAME'] ?? NULL,
-                'aptdia'    => isset($input['APTDIA']) ? intval($input['APTDIA']) : NULL,
-                'focallen'  => isset($input['FOCALLEN']) ? intval($input['FOCALLEN']) : NULL,
-                'comment'   => $input['COMMENT'] ?? NULL,
-                'telescop'  => $input['TELESCOP'] ?? NULL,
-                'observer'  => $input['OBSERVER'] ?? NULL,
-                'instrume'  => $input['INSTRUME'] ?? NULL,
-                'pixsize1'  => isset($input['PIXSIZE1']) ? floatval($input['PIXSIZE1']) : NULL,
-                'pixsize2'  => isset($input['PIXSIZE2']) ? floatval($input['PIXSIZE2']) : NULL,
-                'ccd_temp'  => isset($input['CCD_TEMP']) ? floatval($input['CCD_TEMP']) : NULL,
-                'offset'    => isset($input['OFFSET']) ? intval($input['OFFSET']) : NULL,
-                'gain'      => isset($input['GAIN']) ? intval($input['GAIN']) : NULL,
-                'scale'     => isset($input['SCALE']) ? floatval($input['SCALE']) : NULL,
-                'date_obs'  => $input['DATE_OBS'] ?? NULL,
-                'equinox'   => $input['EQUINOX'] ?? NULL,
+                'simple'    => isset($input['SIMPLE']) ? intval($input['SIMPLE']) : null,
+                'bitpix'    => isset($input['BITPIX']) ? intval($input['BITPIX']) : null,
+                'xbinning'  => isset($input['XBINNING']) ? intval($input['XBINNING']) : null,
+                'ybinning'  => isset($input['YBINNING']) ? intval($input['YBINNING']) : null,
+                'exptime'   => isset($input['EXPTIME']) ? intval($input['EXPTIME']) : null,
+                'frame'     => $input['FRAME'] ?? null,
+                'aptdia'    => isset($input['APTDIA']) ? intval($input['APTDIA']) : null,
+                'focallen'  => isset($input['FOCALLEN']) ? intval($input['FOCALLEN']) : null,
+                'comment'   => $input['COMMENT'] ?? null,
+                'telescop'  => $input['TELESCOP'] ?? null,
+                'observer'  => $input['OBSERVER'] ?? null,
+                'instrume'  => $input['INSTRUME'] ?? null,
+                'pixsize1'  => isset($input['PIXSIZE1']) ? floatval($input['PIXSIZE1']) : null,
+                'pixsize2'  => isset($input['PIXSIZE2']) ? floatval($input['PIXSIZE2']) : null,
+                'ccd_temp'  => isset($input['CCD_TEMP']) ? floatval($input['CCD_TEMP']) : null,
+                'offset'    => isset($input['OFFSET']) ? intval($input['OFFSET']) : null,
+                'gain'      => isset($input['GAIN']) ? intval($input['GAIN']) : null,
+                'scale'     => isset($input['SCALE']) ? floatval($input['SCALE']) : null,
+                'date_obs'  => $input['DATE_OBS'] ?? null,
+                'equinox'   => $input['EQUINOX'] ?? null,
                 'filter'    => $input['FILTER'] ?? 'Luminance',
                 'dec'       => floatval($input['DEC']),
                 'ra'        => floatval($input['RA']),
-                'object'    => $input['OBJECT'] ?? NULL,
-                'objctdec'  => $input['OBJCTDEC'] ?? NULL,
-                'objctra'   => $input['OBJCTRA'] ?? NULL,
+                'object'    => $input['OBJECT'] ?? null,
+                'objctdec'  => $input['OBJCTDEC'] ?? null,
+                'objctra'   => $input['OBJCTRA'] ?? null,
                 'sitelong'  => floatval($input['SITELONG']),
                 'sitelat'   => floatval($input['SITELAT']),
                 'bzero'     => isset($input['BZERO']) ? intval($input['BZERO']) : 0,
-                'extend'    => $input['EXTEND'] ?? NULL,
+                'extend'    => $input['EXTEND'] ?? null,
                 'airmass'   => floatval($input['AIRMASS']),
 
-                'star_count'     => isset($input['STARS_COUNT']) ? intval($input['STARS_COUNT']) : NULL,
-                'sky_background' => isset($input['SKY_BACKGROUND']) ? floatval($input['SKY_BACKGROUND']) : NULL,
-                'devitation'     => isset($input['DEVIATION']) ? floatval($input['DEVIATION']) : NULL,
+                'star_count'     => isset($input['STARS_COUNT']) ? intval($input['STARS_COUNT']) : null,
+                'sky_background' => isset($input['SKY_BACKGROUND']) ? floatval($input['SKY_BACKGROUND']) : null,
+                'devitation'     => isset($input['DEVIATION']) ? floatval($input['DEVIATION']) : null,
 
-                'sigma' => isset($input['SIGMA']) ? floatval($input['SIGMA']) : NULL,
-                'hfr'   => isset($input['MEAN_FWHM']) ? floatval($input['MEAN_FWHM']) : NULL,
-                'fwhm'  => isset($input['MEAN_SNR']) ? floatval($input['MEAN_SNR']) : NULL,
+                'sigma' => isset($input['SIGMA']) ? floatval($input['SIGMA']) : null,
+                'hfr'   => isset($input['MEAN_FWHM']) ? floatval($input['MEAN_FWHM']) : null,
+                'fwhm'  => isset($input['MEAN_SNR']) ? floatval($input['MEAN_SNR']) : null,
             ];
 
             $filesModel->insert($insert);
@@ -111,7 +112,7 @@ class Fits extends ResourceController
             $dirName = UPLOAD_FITS . $key . '/';
 
             if (!is_dir($dirName)) {
-                mkdir($dirName,0777, TRUE);
+                mkdir($dirName,0777, true);
             }
 
             if (! $file->hasMoved()) {
