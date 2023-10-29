3f66a86cf4b57277a1b837822c4e7c9dc817f6c8
Misha Topchilo <miksoft.tm@gmail.com>
2023-10-29 10:25:01 -0700

    Implemented UI tests for FilterList (#105)
    Co-authored-by: Misha Topchilo <20854498+miksrv@users.noreply.github.com>

---
+++ client/components/filter-list/FilterList.test.tsx
@@ -0,0 +1,52 @@
+import { TFilters } from '@/api/types'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+
+import FilterList from './FilterList'
+
+describe('FilterList Component', () => {
+    it('renders without errors when filters are provided', () => {
+        const filters: TFilters = {
+            hydrogen: {
+                exposure: 300,
+                frames: 5
+            },
+            luminance: {
+                exposure: 600,
+                frames: 10
+            }
+        }
+
+        render(<FilterList filters={filters} />)
+
+        expect(screen.getByText('luminance')).toBeInTheDocument()
+        expect(screen.getByText('hydrogen')).toBeInTheDocument()
+        expect(screen.getByText('10 минут (10 кадров)')).toBeInTheDocument()
+        expect(screen.getByText('5 минут (5 кадров)')).toBeInTheDocument()
+        expect(screen.queryByText('filter3')).toBeNull()
+    })
+
+    it('renders nothing when no filters are provided', () => {
+        render(<FilterList />)
+        expect(screen.queryByRole('ul')).toBeNull()
+    })
+
+    it('renders correct text when filters have different values', () => {
+        const filters: TFilters = {
+            hydrogen: {
+                exposure: 60,
+                frames: 1
+            },
+            luminance: {
+                exposure: 600,
+                frames: 10
+            }
+        }
+
+        render(<FilterList filters={filters} />)
+
+        expect(screen.getByText('10 минут (10 кадров)')).toBeInTheDocument()
+        expect(screen.getByText('1 минута (1 кадр)')).toBeInTheDocument()
+    })
+})
