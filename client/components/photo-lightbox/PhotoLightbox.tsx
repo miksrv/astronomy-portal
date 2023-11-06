6bb95de3eb2a3661eb1680926e64ba74a062635f
Misha Topchilo <miksoft.tm@gmail.com>
2023-11-06 10:30:25 -0800

    Implemented Lightbox on the account page (#117)
    * Implemented Lightbox on the about page
    
    * Update package.json
    
    ---------
    
    Co-authored-by: Misha Topchilo <20854498+miksrv@users.noreply.github.com>

---
+++ client/components/photo-lightbox/PhotoLightbox.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import React from 'react'
+import Lightbox from 'react-image-lightbox'
+
+interface PhotoLightboxProps {
+    photos?: string[]
+    thumbnail?: string[]
+    photoIndex?: number
+    showLightbox?: boolean
+    onCloseLightBox?: () => void
+    onChangeIndex?: (index: number) => void
+}
+
+const PhotoLightbox: React.FC<PhotoLightboxProps> = ({
+    photos,
+    thumbnail,
+    photoIndex = 0,
+    showLightbox,
+    onCloseLightBox,
+    onChangeIndex
+}) => (
+    <>
+        {showLightbox && !!photos?.length && (
+            <Lightbox
+                mainSrc={photos?.[photoIndex]}
+                nextSrc={photos?.[(photoIndex + 1) % (photos.length || 0)]}
+                prevSrc={
+                    photos?.[
+                        (photoIndex + (photos.length || 0) - 1) %
+                            (photos.length || 0)
+                    ]
+                }
+                mainSrcThumbnail={photos?.[photoIndex]}
+                prevSrcThumbnail={
+                    thumbnail?.[
+                        (photoIndex + (photos.length || 0) - 1) %
+                            (photos.length || 0)
+                    ]
+                }
+                nextSrcThumbnail={
+                    thumbnail?.[(photoIndex + 1) % (photos.length || 0)]
+                }
+                onCloseRequest={() => onCloseLightBox?.()}
+                onMovePrevRequest={() =>
+                    onChangeIndex?.(
+                        (photoIndex + (photos.length || 0) - 1) %
+                            (photos.length || 0)
+                    )
+                }
+                onMoveNextRequest={() =>
+                    onChangeIndex?.((photoIndex + 1) % (photos.length || 0))
+                }
+            />
+        )}
+    </>
+)
+
+export default PhotoLightbox
