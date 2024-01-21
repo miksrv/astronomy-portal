f2d7ce98a1a9cce210c5eb86eb7431a7300d5a85
Misha Topchilo <miksoft.tm@gmail.com>
2024-01-21 12:08:41 -0800

    Refactoring BlogPage and ObjectPage UI component (#135)
    Co-authored-by: Misha Topchilo <20854498+miksrv@users.noreply.github.com>

--- client/pages/blog.tsx
+++ client/pages/blog.tsx
@@ -1,53 +1,55 @@
-import {
-    blogGetList,
-    getRunningQueriesThunk,
-    useBlogGetListQuery
-} from '@/api/api'
+import { blogGetList, getRunningQueriesThunk } from '@/api/api'
 import { wrapper } from '@/api/store'
+import { TBlog } from '@/api/types'
 import { sliceText } from '@/functions/helpers'
-import { NextPage } from 'next'
+import { GetServerSidePropsResult, NextPage } from 'next'
 import { NextSeo } from 'next-seo'
 import React from 'react'
 
 import BlogPage from '@/components/blog-page'
 import { getMediaFromPost, postPerPage } from '@/components/blog-page/BlogPage'
 
+interface BlogPageProps {
+    blogItems: TBlog[]
+    blogTotal: number
+}
+
+const Blog: NextPage<BlogPageProps> = ({ blogItems, blogTotal }) => (
+    <main>
+        <NextSeo
+            title={'Астрономический блог'}
+            description={sliceText(blogItems?.[0]?.text ?? '', 200)}
+            openGraph={{
+                images: getMediaFromPost(blogItems?.[0]),
+                locale: 'ru'
+            }}
+        />
+        <BlogPage
+            page={1}
+            posts={blogItems}
+            total={blogTotal}
+        />
+    </main>
+)
+
 export const getServerSideProps = wrapper.getServerSideProps(
-    (store) => async () => {
-        store.dispatch(blogGetList.initiate({ limit: postPerPage, offset: 0 }))
+    (store) => async (): Promise<GetServerSidePropsResult<BlogPageProps>> => {
+        const { data } = await store.dispatch(
+            blogGetList.initiate({
+                limit: postPerPage,
+                offset: 0
+            })
+        )
 
         await Promise.all(store.dispatch(getRunningQueriesThunk()))
 
         return {
-            props: { object: {} }
+            props: {
+                blogItems: data?.items || [],
+                blogTotal: data?.total || 0
+            }
         }
     }
 )
-
-const Blog: NextPage = () => {
-    const { data, isLoading } = useBlogGetListQuery({
-        limit: postPerPage,
-        offset: 0
-    })
-
-    return (
-        <main>
-            <NextSeo
-                title={'Астрономический блог'}
-                description={sliceText(data?.items?.[0]?.text ?? '', 200)}
-                openGraph={{
-                    images: getMediaFromPost(data?.items?.[0]),
-                    locale: 'ru'
-                }}
-            />
-            <BlogPage
-                page={1}
-                loading={isLoading}
-                posts={data?.items}
-                total={data?.total}
-            />
-        </main>
-    )
-}
 
 export default Blog
