ddeb8a8c4a2662a82fddde1584b06eab5b66693d
Misha Topchilo <miksoft.tm@gmail.com>
2024-02-14 15:24:31 -0800

    Changed UI deploy VPS server (#139)
    Co-authored-by: Misha Topchilo <20854498+miksrv@users.noreply.github.com>

---
+++ client/ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+        {
+            args: 'start',
+            autorestart: true,
+            env: {
+                NODE_ENV: 'production'
+            },
+            instances: 1,
+            max_memory_restart: '256Mb',
+            name: 'astro.miksoft.pro',
+            script: 'server.js',
+            watch: false
+        }
+    ]
+}
