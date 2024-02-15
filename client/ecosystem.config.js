452c8543426309ab077f2d2443d846db9b23768f
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-02-14 19:37:23 -0800

    Updated CI/CD

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
                 NODE_ENV: 'production'
             },
             instances: 1,
-            max_memory_restart: '256Mb',
+            max_memory_restart: '256M',
             name: 'astro.miksoft.pro',
             script: 'server.js',
             watch: false
