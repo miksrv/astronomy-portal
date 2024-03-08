e622bb09e29cb2d5e29762b8cc14a465b3215873
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-03-08 13:44:07 -0800

    Removed unused code

--- server/app/Controllers/Auth.php
+++ server/app/Controllers/Auth.php
@@ -5,7 +5,6 @@ use App\Models\UsersModel;
 use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\RESTful\ResourceController;
-use CodeIgniter\Validation\Exceptions\ValidationException;
 use Config\Services;
 use Exception;
 use ReflectionException;
@@ -76,24 +75,6 @@ class Auth extends ResourceController {
      */
     public function validateRequest($input, array $rules, array $messages =[]): bool {
         $this->validator = Services::Validation()->setRules($rules);
-        // If you replace the $rules array with the name of the group
-        if (is_string($rules)) {
-            $validation = config('Validation');
-
-            // If the rule wasn't found in the \Config\Validation, we
-            // should throw an exception so the developer can find it.
-            if (!isset($validation->$rules)) {
-                throw ValidationException::forRuleNotFound($rules);
-            }
-
-            // If no error message is defined, use the error message in the Config\Validation file
-            if (!$messages) {
-                $errorName = $rules . '_errors';
-                $messages = $validation->$errorName ?? [];
-            }
-
-            $rules = $validation->$rules;
-        }
 
         return $this->validator->setRules($rules, $messages)->run($input);
     }
