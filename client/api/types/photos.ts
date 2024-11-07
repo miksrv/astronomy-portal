2d44f233cb73cb00d52ea90b235328a8a91386bf
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-11-07 14:00:43 -0800

    Added API Languages files (for en and ru)

---
+++ client/api/types/photos.ts
@@ -0,0 +1,41 @@
+import { ApiModel } from '@/api'
+
+export interface Response {
+    count?: number
+    items?: ApiModel.Photo[]
+}
+
+export interface Request {
+    object?: string
+    limit?: number
+    order?: 'random' | keyof ApiModel.Photo
+}
+
+// /* Item */
+// export interface ResItem extends ApiModel.Photo {}
+
+// export interface ReqList {
+//     object?: string
+//     limit?: number
+//     order?: 'random' | keyof ApiModel.Photo
+// }
+//
+// /* Set */
+// export interface ResSet extends ApiModel.Photo {}
+//
+// export interface ReqSet {
+//     id: number
+//     object: string
+//     date: string
+//     author_id: number
+//     image_name: string
+//     image_ext: string
+//     filters?: ApiModel.Filter.ListItems
+// }
+//
+// /* Upload */
+// export interface ResUpload {
+//     image_name: string
+//     image_ext: string
+//     image_size: number
+// }
---
+++ server/app/Language/en/General.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'serverError' => 'Internal Server Error'
+];
