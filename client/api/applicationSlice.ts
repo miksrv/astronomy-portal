5eb97cecf06224da3d6c1ad9c57db1932aa7a2b9
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-11-10 22:13:02 -0800

    Update applicationSlice.ts

--- client/api/applicationSlice.ts
+++ client/api/applicationSlice.ts
@@ -1,16 +1,36 @@
+import * as LocalStorage from '@/functions/localstorage'
 import { ApiModel } from '@/api'
+import { LOCAL_STORAGE } from '@/functions/constants'
 import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 
-type TApplicationSlice = {
+import i18Config from '../next-i18next.config'
+
+interface ApplicationSliceProps {
     isActiveFormCatalog: boolean
     isActiveFormPhoto: boolean
     editableItemCatalog?: ApiModel.Catalog
     editableItemPhoto?: ApiModel.Photo
+
+    showOverlay?: boolean
+    headerHeight?: number
+    footerHeight?: number
+    locale?: 'ru' | 'en' | string
 }
 
-const initialState: TApplicationSlice = {
+export const getStorageLocale = (): string | undefined =>
+    typeof window !== 'undefined'
+        ? LocalStorage.getItem(LOCAL_STORAGE.LOCALE as any) ??
+          i18Config.i18n.defaultLocale
+        : i18Config.i18n.defaultLocale
+
+const initialState: ApplicationSliceProps = {
+    footerHeight: 0,
+    headerHeight: 0,
+
     isActiveFormCatalog: false,
-    isActiveFormPhoto: false
+    isActiveFormPhoto: false,
+    locale: getStorageLocale(),
+    showOverlay: false
 }
 
 const applicationSlice = createSlice({
@@ -42,11 +62,35 @@ const applicationSlice = createSlice({
             if (action.payload === false) {
                 state.editableItemPhoto = undefined
             }
+        },
+
+        setFooterHeight: (state, { payload }: PayloadAction<number>) => {
+            state.footerHeight = payload
+        },
+        setHeaderHeight: (state, { payload }: PayloadAction<number>) => {
+            state.headerHeight = payload
+        },
+        setLocale: (
+            state,
+            { payload }: PayloadAction<'ru' | 'en' | string>
+        ) => {
+            state.locale = payload
+        },
+        toggleOverlay: (state, { payload }: PayloadAction<boolean>) => {
+            state.showOverlay = payload
         }
     }
 })
 
-export const { editCatalog, editPhoto, openFormCatalog, openFormPhoto } =
-    applicationSlice.actions
+export const {
+    editCatalog,
+    editPhoto,
+    openFormCatalog,
+    openFormPhoto,
+    setFooterHeight,
+    setHeaderHeight,
+    toggleOverlay,
+    setLocale
+} = applicationSlice.actions
 
 export default applicationSlice.reducer
