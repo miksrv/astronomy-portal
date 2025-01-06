26c4885f9878b4c0f049fceb2522e59f60ba3423
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2025-01-06 12:44:26 -0800

    Implemented UI Breadcrumb component

---
+++ client/ui/breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,56 @@
+import Link from 'next/link'
+import React from 'react'
+import { cn } from 'simple-react-ui-kit'
+
+import styles from './styles.module.sass'
+
+export type BreadcrumbLink = {
+    link: string
+    text: string
+}
+
+export interface BreadcrumbsProps {
+    homePageTitle?: string
+    currentPage?: string
+    className?: string
+    links?: BreadcrumbLink[]
+}
+
+const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
+    homePageTitle,
+    links,
+    className,
+    currentPage
+}) => (
+    <ul
+        aria-label={'breadcrumb'}
+        className={cn(className, styles.breadcrumbs)}
+    >
+        {!!homePageTitle?.length && (
+            <li>
+                <Link
+                    color={'inherit'}
+                    href={'/'}
+                    title={homePageTitle}
+                >
+                    {homePageTitle}
+                </Link>
+            </li>
+        )}
+        {!!links?.length &&
+            links.map(({ link, text }) => (
+                <li key={link}>
+                    <Link
+                        href={link}
+                        color={'inherit'}
+                        title={text}
+                    >
+                        {text}
+                    </Link>
+                </li>
+            ))}
+        {currentPage && <li>{currentPage}</li>}
+    </ul>
+)
+
+export default Breadcrumbs
