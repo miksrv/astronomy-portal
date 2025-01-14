bbbc4d3889a9fcb17ddbe025c7992dbf47fc81de
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2025-01-14 13:58:38 -0800

    Implemented UI Main Sections Component

---
+++ client/components/main-sections/MainSectionAstrophotos.tsx
@@ -0,0 +1,73 @@
+import { ApiModel } from '@/api'
+import { createMediumPhotoUrl, createPhotoTitle } from '@/tools/photos'
+import { useTranslation } from 'next-i18next'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useMemo } from 'react'
+import { cn } from 'simple-react-ui-kit'
+
+import styles from './styles.module.sass'
+
+interface MainSectionStargazingProps {
+    photos?: ApiModel.Photo[]
+}
+
+const MainSectionStargazing: React.FC<MainSectionStargazingProps> = ({
+    photos
+}) => {
+    const { t } = useTranslation()
+
+    const randomPhotos = useMemo(
+        () => [...(photos || [])]?.sort(() => 0.5 - Math.random())?.slice(0, 3),
+        [photos]
+    )
+
+    return (
+        <section className={styles.section}>
+            <div
+                className={styles.background}
+                style={{
+                    backgroundImage: 'url(/images/astrophoto.jpeg)'
+                }}
+            />
+            <div
+                className={cn(
+                    styles.centralContainer,
+                    styles.photosContainer,
+                    'animate animate-slide-up'
+                )}
+            >
+                {randomPhotos?.map((photo) => (
+                    <Link
+                        key={photo.id}
+                        href={`/photos/${photo.id}`}
+                        title={createPhotoTitle(photo, t)}
+                        className={styles.photoItem}
+                    >
+                        <Image
+                            src={createMediumPhotoUrl(photo)}
+                            alt={photo.id}
+                            fill={true}
+                        />
+                    </Link>
+                ))}
+            </div>
+
+            <div className={styles.bottomContainer}>
+                <h2 className={'animate'}>{t('astrophoto')}</h2>
+                <p className={'animate'}>
+                    {t('astrophoto-section-description')}
+                </p>
+                <Link
+                    href={'/photos'}
+                    title={t('astrophoto')}
+                    className={'animate'}
+                >
+                    {t('read-more')}
+                </Link>
+            </div>
+        </section>
+    )
+}
+
+export default MainSectionStargazing
---
+++ client/components/main-sections/index.ts
@@ -0,0 +1,11 @@
+import MainSectionAstrophotos from './MainSectionAstrophotos'
+import MainSectionObservatory from './MainSectionObservatory'
+import MainSectionStargazing from './MainSectionStargazing'
+
+const MainSection = {
+    Stargazing: MainSectionStargazing,
+    Astrophotos: MainSectionAstrophotos,
+    Observatory: MainSectionObservatory
+}
+
+export default MainSection
