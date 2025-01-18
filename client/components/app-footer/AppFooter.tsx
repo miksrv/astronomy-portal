376cadaa190bac46046c1d02d424da9a5393176c
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2025-01-18 12:51:35 -0800

    Finished UI AppFooter and AppToolbar components

--- client/components/app-footer/AppFooter.tsx
+++ client/components/app-footer/AppFooter.tsx
@@ -1,41 +1,49 @@
 import { formatDate } from '@/functions/helpers'
 import packageInfo from '@/package.json'
 import Image from 'next/image'
-import React from 'react'
+import React, { forwardRef } from 'react'
 
 import { update } from '@/update'
 
 import styles from './styles.module.sass'
 
-const AppFooter: React.FC = () => {
-    return (
-        <footer
-            className={styles.footer}
-            data-testid={'footer'}
-        >
-            <div>Powered by Arduino, PHP + MySQL, NextJS + TS + Redux.</div>
-            <div>
-                Copyright ©
-                <a
-                    href={'https://miksoft.pro'}
-                    className={styles.copyrightLink}
-                    title={''}
-                >
-                    <Image
-                        className={styles.copyrightImage}
-                        src={'https://miksoft.pro/favicon.ico'}
-                        alt={''}
-                        width={11}
-                        height={11}
-                    />
-                    {'Mik'}
-                </a>{' '}
-                {formatDate(new Date(), 'YYYY')}, Version{' '}
-                <span>{packageInfo.version}</span>{' '}
-                <span>({formatDate(update, 'DD.MM.YYYY, HH:mm')})</span>
-            </div>
-        </footer>
-    )
-}
+const AppFooter = forwardRef<HTMLDivElement>((props, ref) => (
+    <footer
+        className={styles.footer}
+        ref={ref}
+    >
+        <div>Powered by Arduino, PHP + MySQL, NextJS + TS + Redux.</div>
+        <div>
+            Copyright ©
+            <a
+                href={'https://miksoft.pro'}
+                className={styles.link}
+                title={''}
+            >
+                <Image
+                    className={styles.copyrightImage}
+                    src={'https://miksoft.pro/favicon.ico'}
+                    alt={''}
+                    width={11}
+                    height={11}
+                />
+                {'Mik'}
+            </a>
+            {formatDate(new Date(), 'YYYY')}, {'v'}
+            {packageInfo.version}
+            <a
+                href={'https://github.com/miksrv/astronomy-portal'}
+                rel={'nofollow noindex'}
+                className={styles.link}
+                title={''}
+            >
+                {'GitHub'}
+            </a>
+            ({formatDate(update, 'DD.MM.YYYY, HH:mm')})
+        </div>
+    </footer>
+))
+
+AppFooter.displayName = 'AppFooter'
 
 export default AppFooter
--- client/components/app-toolbar/AppToolbar.tsx
+++ client/components/app-toolbar/AppToolbar.tsx
@@ -1,42 +1,44 @@
 import '@/api/applicationSlice'
 import Breadcrumbs, { BreadcrumbLink, BreadcrumbsProps } from '@/ui/breadcrumbs'
 import { useTranslation } from 'next-i18next'
-import React from 'react'
+import React, { forwardRef } from 'react'
 
 import styles from './styles.module.sass'
 
-interface AppHeaderProps
+interface AppToolbarProps
     extends Pick<BreadcrumbsProps, 'currentPage' | 'links'> {
     title?: string
     children?: React.ReactNode
     breadcrumbs?: BreadcrumbLink[]
 }
 
-const AppToolbar: React.FC<AppHeaderProps> = ({
-    title,
-    links,
-    currentPage,
-    children
-}) => {
-    const { t } = useTranslation()
+const AppToolbar = forwardRef<HTMLDivElement, AppToolbarProps>(
+    ({ title, links, currentPage, children }, ref) => {
+        const { t } = useTranslation()
 
-    return (
-        <div className={styles.toolbarHeader}>
-            <div className={styles.toolbarTitle}>
-                <h1>{title}</h1>
-                {(links || currentPage) && (
-                    <Breadcrumbs
-                        links={links}
-                        currentPage={currentPage}
-                        homePageTitle={t('look-at-the-stars')}
-                    />
+        return (
+            <div
+                className={styles.toolbarHeader}
+                ref={ref}
+            >
+                <div className={styles.toolbarTitle}>
+                    <h1>{title}</h1>
+                    {(links || currentPage) && (
+                        <Breadcrumbs
+                            links={links}
+                            currentPage={currentPage}
+                            homePageTitle={t('look-at-the-stars')}
+                        />
+                    )}
+                </div>
+                {children && (
+                    <div className={styles.toolbarActions}>{children}</div>
                 )}
             </div>
-            {children && (
-                <div className={styles.toolbarActions}>{children}</div>
-            )}
-        </div>
-    )
-}
+        )
+    }
+)
+
+AppToolbar.displayName = 'AppToolbar'
 
 export default AppToolbar
