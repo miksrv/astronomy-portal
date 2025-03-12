8b7b802d7708bfdcf178e4d1bdaf09b9a8d7d809
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2025-03-12 11:53:20 -0700

    Added new link for UI about page (to observatory overview)

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -66,6 +66,12 @@ const galleryStargazing = [
     photoStargazing8
 ]
 
+const allPhotos = [
+    ...galleryAboutMe,
+    ...galleryStargazing,
+    ...galleryObservatory
+]
+
 const contributors1: string[] = [
     'Марина Станиславовна С.',
     'Игнат Евгеньевич П. (Хорошим людям, делающим хорошие дела)',
@@ -110,12 +116,6 @@ const AboutPage: NextPage<AboutPageProps> = () => {
 
     const [showLightbox, setShowLightbox] = useState<boolean>(false)
     const [photoIndex, setPhotoIndex] = useState<number>(0)
-
-    const allPhotos = [
-        ...galleryAboutMe,
-        ...galleryStargazing,
-        ...galleryObservatory
-    ]
 
     const handlePhotoClick = (index: number) => {
         setPhotoIndex(index)
@@ -242,6 +242,13 @@ const AboutPage: NextPage<AboutPageProps> = () => {
                 </h2>
                 <p style={{ marginTop: 0 }}>
                     {t('about-page.observatory-intro')}
+                    <Link
+                        href={'/objects'}
+                        title={t('about-page.observatory-intro-link')}
+                    >
+                        {t('about-page.observatory-intro-link')}
+                    </Link>
+                    {'.'}
                 </p>
                 <Gallery
                     photos={galleryObservatory}
