b3f50b4c6de3df71254306571124d3dae48bfc56
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2025-03-17 13:57:57 -0700

    Improved and refactoring UI API models

--- client/api/types/weather.ts
+++ client/api/types/weather.ts
@@ -1,20 +1,9 @@
 import { ApiModel } from '@/api'
 
-/* Current */
-export interface ResCurrent {
-    timestamp: {
-        server: number
-        update: number
-    }
-    conditions: ApiModel.Weather
-}
-
 /* Statistic */
-export interface ReqStatistic {
-    period?: string
+export interface RequestHistory {
+    start_date: string
+    end_date: string
 }
 
-export interface ResStatistic {
-    date: string
-    weather: ApiModel.Weather[]
-}
+export type ResponseHistory = ApiModel.Weather[]
