91836567ae608d352e1c4306a75d155748bbe81f
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2025-03-25 11:57:38 -0700

    Updated UI Unit Tests Config

--- client/setupTests.config.tsx
+++ client/setupTests.config.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Provider } from 'react-redux'
 
+// import { Provider } from 'react-redux'
 import '@testing-library/jest-dom'
 
-import { API } from '@/api/api'
-import { reducer, RootState } from '@/api/store'
-import { configureStore } from '@reduxjs/toolkit'
-import { render } from '@testing-library/react'
+// import { API } from '@/api/api'
+// import { reducer, RootState } from '@/api/store'
+// import { configureStore } from '@reduxjs/toolkit'
+// import { render } from '@testing-library/react'
 
-type WrapperProps = {
-    children: React.ReactNode
-}
+// type WrapperProps = {
+//     children: React.ReactNode
+// }
 
 jest.mock('next/image', () => ({
     __esModule: true,
@@ -18,18 +18,18 @@ jest.mock('next/image', () => ({
     default: (props: any) => <img {...props} />
 }))
 
-export const testStore = (state: Partial<RootState>) => {
-    return configureStore({
-        middleware: (gDM) => gDM().concat(API.middleware) as any,
-        preloadedState: state,
-        reducer: reducer as any
-    })
-}
+// export const testStore = (state: Partial<RootState>) => {
+//     return configureStore({
+//         middleware: (gDM) => gDM().concat(API.middleware) as any,
+//         preloadedState: state,
+//         reducer: reducer as any
+//     })
+// }
 
-export const renderWithStore = (component: React.ReactElement, initialState: any) => {
-    const Wrapper: React.FC<WrapperProps> = ({ children }) => (
-        <Provider store={testStore(initialState)}>{children}</Provider>
-    )
-
-    return render(component, { wrapper: Wrapper })
-}
+// export const renderWithStore = (component: React.ReactElement, initialState: any) => {
+//     const Wrapper: React.FC<WrapperProps> = ({ children }) => (
+//         <Provider store={testStore(initialState)}>{children}</Provider>
+//     )
+//
+//     return render(component, { wrapper: Wrapper })
+// }
