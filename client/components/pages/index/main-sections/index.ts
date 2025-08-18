67b62861528ebef49885a7eb9595fe86c97c23e4
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2025-08-17 21:19:13 -0700

    Refactoring UI index page components

---
+++ client/components/pages/index/main-sections/index.ts
@@ -0,0 +1,9 @@
+import { MainSectionAstrophotos } from './MainSectionAstrophotos'
+import { MainSectionObservatory } from './MainSectionObservatory'
+import { MainSectionStargazing } from './MainSectionStargazing'
+
+export const MainSection = {
+    Stargazing: MainSectionStargazing,
+    Astrophotos: MainSectionAstrophotos,
+    Observatory: MainSectionObservatory
+}
