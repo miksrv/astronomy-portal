af96716e49c2cdac55c6c30b2504b623563d2d1a
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2025-08-21 21:00:28 -0700

    Create i18next-scanner.config.js

---
+++ client/i18next-scanner.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    input: [
+        "**/*.{js,jsx,ts,tsx}",
+        "!**/node_modules/**",
+        "!**/dist/**"
+    ],
+    output: "./public/locales",
+
+    options: {
+        debug: false,
+        removeUnusedKeys: true,
+        sort: true,
+        lngs: ["ru", "en"],
+        defaultLng: "ru",
+        defaultNs: "common",
+        resource: {
+            loadPath: "public/locales/{{lng}}/{{ns}}.json",
+            savePath: "public/locales/{{lng}}/{{ns}}.json"
+        },
+        interpolation: {
+            prefix: "{{",
+            suffix: "}}"
+        }
+    },
+
+    transform: function customTransform(file, enc, done) {
+        const parser = this.parser;
+        const content = file.contents.toString("utf8");
+
+        parser.parseFuncFromString(content, { list: ["t"] }, (key, options) => {
+            parser.set(key, {
+                defaultValue: options.defaultValue || key
+            });
+        });
+
+        done();
+    }
+};
