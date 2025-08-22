4e049b9156ecedd5434ec8e90b8e484d28c153fd
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2025-08-21 21:00:39 -0700

    Refactoring locales keys for ShowMore UI Component

--- client/components/ui/show-more/ShowMore.tsx
+++ client/components/ui/show-more/ShowMore.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { Button, cn } from 'simple-react-ui-kit'
 
+import { useTranslation } from 'next-i18next'
+
 import styles from './styles.module.sass'
 
 interface ShowMoreProps {
@@ -8,6 +10,7 @@ interface ShowMoreProps {
 }
 
 export const ShowMore: React.FC<ShowMoreProps> = ({ children }) => {
+    const { t } = useTranslation()
     const [isExpanded, setIsExpanded] = useState(false)
 
     return (
@@ -19,7 +22,9 @@ export const ShowMore: React.FC<ShowMoreProps> = ({ children }) => {
                 size={'medium'}
                 onClick={() => setIsExpanded(!isExpanded)}
             >
-                {isExpanded ? 'Показать меньше' : 'Показать больше'}
+                {isExpanded
+                    ? t('components.ui.show-more.show-less', { defaultValue: 'Показать меньше' })
+                    : t('components.ui.show-more.show-more', { defaultValue: 'Показать больше' })}
             </Button>
         </div>
     )
