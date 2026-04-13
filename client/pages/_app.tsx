517167eefe8977789ad1607c371f3b6f202022d1
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2026-04-13 14:28:18 -0700

    Move analytics to Next.js Script and add Document
    Replace raw dangerouslySetInnerHTML analytics injection with Next.js <Script> for Yandex (loaded afterInteractive) and keep a noscript image fallback. Add referrer and url to Yandex init and avoid inline HTML injection. Also introduce a basic custom _document.tsx to render Html/Head/Main/NextScript.

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -108,11 +108,38 @@ const App = ({ Component, pageProps }: AppProps) => {
             </Provider>
 
             {process.env.NODE_ENV === 'production' && (
-                <div
-                    dangerouslySetInnerHTML={{
-                        __html: '<!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(93471741, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/93471741" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter --><!-- Google tag (gtag.js) --><script async src="https://www.googletagmanager.com/gtag/js?id=G-BGBKSHELMF"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag("js", new Date());gtag("config", "G-BGBKSHELMF");</script>'
-                    }}
-                />
+                <>
+                    {/* Yandex.Metrika counter */}
+                    <Script
+                        id={'yandex-metrika'}
+                        strategy={'afterInteractive'}
+                    >
+                        {`
+                            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                            m[i].l=1*new Date();
+                            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                            ym(93471741, "init", {
+                                clickmap:true,
+                                referrer: document.referrer,
+                                url: location.href,
+                                accurateTrackBounce:true,
+                                trackLinks:true
+                            });
+                        `}
+                    </Script>
+                    <noscript>
+                        <div>
+                            <img
+                                src={'https://mc.yandex.ru/watch/93471741'}
+                                style={{ position: 'absolute', left: '-9999px' }}
+                                alt={''}
+                            />
+                        </div>
+                    </noscript>
+                </>
             )}
         </>
     )
