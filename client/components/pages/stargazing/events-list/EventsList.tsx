f099219efb88ec9498a6f415d93a5f1775837cc5
Misha Topchilo <miksoft.tm@gmail.com>
2026-08-17 14:37:11 -0700

    Inline EventsListItem into EventsList
    Remove the intermediate EventsListItem wrapper component and inline its logic directly into EventsList, moving the translation and permission checks up to the parent map call.

--- client/components/pages/stargazing/events-list/EventsList.tsx
+++ client/components/pages/stargazing/events-list/EventsList.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-import { ApiModel } from '@/api'
+import { useTranslation } from 'next-i18next/pages'
 
-import { EventsListItem } from './EventsListItem'
+import { ApiModel, useAppSelector } from '@/api'
+import { hasPermission } from '@/utils/permissions'
+
+import { EventRow } from '../event-row'
 
 import styles from './styles.module.sass'
 
@@ -10,13 +13,33 @@ interface EventsListProps {
     events?: ApiModel.Event[]
 }
 
-export const EventsList: React.FC<EventsListProps> = ({ events }) => (
-    <div className={styles.eventsList}>
-        {events?.map((place) => (
-            <EventsListItem
-                key={place.id}
-                event={place}
-            />
-        ))}
-    </div>
-)
+export const EventsList: React.FC<EventsListProps> = ({ events }) => {
+    const { t } = useTranslation()
+
+    const user = useAppSelector((state) => state.auth?.user)
+
+    const canViewStatistic = hasPermission(user, ApiModel.Permission.EVENTS_STATISTIC)
+
+    return (
+        <div className={styles.eventsList}>
+            {events?.map((event) => (
+                <EventRow
+                    key={event.id}
+                    id={event.id}
+                    title={event.title}
+                    linkTitle={t('components.pages.stargazing.events-list.item-title', 'Астровыезд - {{title}}', {
+                        title: event.title
+                    })}
+                    date={event?.date?.date}
+                    location={event.location}
+                    excerpt={event.excerpt}
+                    coverFileName={event.coverFileName}
+                    coverFileExt={event.coverFileExt}
+                    views={event?.views}
+                    membersCount={event?.members?.total || event?.availableTickets}
+                    statisticHref={canViewStatistic ? `/stargazing/${event.id}/statistic` : undefined}
+                />
+            ))}
+        </div>
+    )
+}
